5e1506f688b14d05392da9afbf9c79b8ef9b9407
Rubin <rubinimeri6@gmail.com>
2023-08-07 21:23:51 +0200

    Create function that gets weather data from API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './style.css';
 
-console.log('hfs');
+async function fetchWeatherData(location) {
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=b4feac5141fe4cff881180840230608&q=${location}`);
+
+  return console.log(response);
+}
+fetchWeatherData('skopje');
