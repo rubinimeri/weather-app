a456046799a9be294c6a4ce2f3546c0bcb3e3962
Rubin <rubinimeri6@gmail.com>
2023-08-07 21:39:57 +0200

    Add search bar for location

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import './style.css';
 
+const form = document.querySelector('form');
+const search = document.getElementById('search');
+
+// Fetch weather data from API by providing location
 async function fetchWeatherData(location) {
   const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=b4feac5141fe4cff881180840230608&q=${location}`);
 
   return response;
 }
 
+// Process the data acquired using .json()
 async function processWeatherData(location) {
   const response = await fetchWeatherData(location);
   const data = await response.json();
@@ -14,3 +19,11 @@ async function processWeatherData(location) {
 }
 
 processWeatherData('skopje');
+
+// Add event listener to form
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+  processWeatherData(location);
+});
