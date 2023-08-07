c25012109130dd2431473c5318ff5b8dea3af6f5
Rubin <rubinimeri6@gmail.com>
2023-08-07 21:30:33 +0200

    Create function that processes data from API

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import './style.css';
 async function fetchWeatherData(location) {
   const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=b4feac5141fe4cff881180840230608&q=${location}`);
 
-  return console.log(response);
+  return response;
 }
-fetchWeatherData('skopje');
+
+async function processWeatherData(location) {
+  const response = await fetchWeatherData(location);
+  const data = await response.json();
+
+  return console.log(data);
+}
+
+processWeatherData('skopje');
