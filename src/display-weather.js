cec45e1dbf00c025031b50841c9d30891535d117
Rubin <rubinimeri6@gmail.com>
2023-08-08 17:12:56 +0200

    Create function that loads the location name

--- src/display-weather.js
+++ src/display-weather.js
@@ -11,6 +11,14 @@ const displayDegrees = async (data) => {
   temp.textContent = tempValue.current.temp_c;
 };
 
+// Display city and country name
+const displayLocationName = async (data) => {
+  const response = await data;
+  const location = document.querySelector('.city');
+
+  location.textContent = `${response.location.name}, ${response.location.country}`;
+};
+
 // Add event listener to form
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -18,5 +26,6 @@ form.addEventListener('submit', (e) => {
   const location = search.value;
   const cityData = processWeatherData(location);
   displayDegrees(cityData);
+  displayLocationName(cityData);
   /* console.log(cityData); */
 });
